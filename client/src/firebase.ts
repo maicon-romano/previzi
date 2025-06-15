1ccefcd68e485dab2ce71c9cd473cb0c2aa338a2
maicon-romano <shardersfx@gmail.com>
2025-06-15 04:15:28 -0300

    Ajuste no código, retomando do zero

--- client/src/firebase.ts
+++ client/src/firebase.ts
@@ -3,12 +3,21 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "default_key",
-  authDomain: `${import.meta.env.VITE_FIREBASE_PROJECT_ID || "previzi-54773"}.firebaseapp.com`,
+  apiKey:
+    import.meta.env.VITE_FIREBASE_API_KEY ||
+    "AIzaSyDmTW3ZuYmR4sjV2Mm983epj4yB-L-Mp-s",
+  authDomain: `${
+    import.meta.env.VITE_FIREBASE_PROJECT_ID || "previzi-54773"
+  }.firebaseapp.com`,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "previzi-54773",
-  storageBucket: `${import.meta.env.VITE_FIREBASE_PROJECT_ID || "previzi-54773"}.firebasestorage.app`,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "default_sender_id",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "default_app_id",
+  storageBucket: `${
+    import.meta.env.VITE_FIREBASE_PROJECT_ID || "previzi-54773"
+  }.firebasestorage.app`,
+  messagingSenderId:
+    import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "default_sender_id",
+  appId:
+    import.meta.env.VITE_FIREBASE_APP_ID ||
+    "1:1054912590243:web:a4677f2390fdc4af3b6a16",
 };
 
 const app = initializeApp(firebaseConfig);
